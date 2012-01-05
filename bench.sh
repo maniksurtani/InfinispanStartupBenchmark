f6df80fb1ba141429bfe71729038e9cad844349b
Manik Surtani <manik@surtani.org>
2012-01-05 19:05:21 +0000

    Update POMs

--- bench.sh
+++ bench.sh
@@ -7,7 +7,7 @@ echo ""
 versions="infinispan-5.0 infinispan-5.1.CR infinispan-5.1.SNAPSHOT"
 for inf in $versions; do
   cd $inf
-  mvn -q -o install -DskipTests=true
+  mvn -q -o install -DskipTests=true -PClusteredTest
   cd ..
 done
 echo ""
